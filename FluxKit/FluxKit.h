907004ca7923f5931ea6c35b2a3fe58800efc66e
Dominique d'Argent <dargent@in.tum.de>
2015-05-16 11:47:19 +0200

    Initial Commit

---
+++ FluxKit/FluxKit.h
@@ -0,0 +1,19 @@
+//
+//  FluxKit.h
+//  FluxKit
+//
+//  Created by Dominique d'Argent on 16/05/15.
+//  Copyright (c) 2015 Dominique d'Argent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FluxKit.
+FOUNDATION_EXPORT double FluxKitVersionNumber;
+
+//! Project version string for FluxKit.
+FOUNDATION_EXPORT const unsigned char FluxKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FluxKit/PublicHeader.h>
+
+
